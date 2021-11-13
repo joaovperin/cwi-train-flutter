0ef9d2fbe2d2f4d72a13c8f234a8ba2778b91712
João Victor Perin <joaovperin@live.com>
2021-11-13 18:20:29 -0300

    messing with colors

---
+++ homeworks/calculator_app/lib/commons/app_colors.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/widgets.dart';
+
+class AppColors {
+  const AppColors._();
+
+  static const Color primary = Color(0xFF00A6FF);
+  static const Color primaryDark = Color(0xFF0082CC);
+  static const Color primaryLight = Color(0xFF00B8FF);
+  static const Color primaryLightest = Color(0xFF00E6FF);
+  static const Color primaryDarkest = Color(0xFF0052CC);
+  
+}
--- homeworks/calculator_app/lib/pages/home_page.dart
+++ homeworks/calculator_app/lib/pages/home_page.dart
@@ -1,3 +1,4 @@
+import 'package:calculator_app/commons/app_colors.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatefulWidget {
@@ -9,10 +10,23 @@ class HomePage extends StatefulWidget {
 
 class _HomePageState extends State<HomePage> {
   int _counter = 0;
+  Color _currentColor = _colors[0];
+
+  static const List<Color> _colors = [
+    AppColors.primary,
+    AppColors.primaryDark,
+    AppColors.primaryLight,
+    AppColors.primaryLightest,
+    AppColors.primaryDarkest,
+  ];
 
   void _incrementCounter() {
     setState(() {
       _counter++;
+      if (_counter >= _colors.length) {
+        _counter = 0;
+      }
+      _currentColor = _colors[_counter];
     });
   }
 
@@ -21,13 +35,32 @@ class _HomePageState extends State<HomePage> {
     return Scaffold(
       appBar: AppBar(
         title: Text('Flutter Demo Home Page'),
+        backgroundColor: _currentColor,
       ),
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+            Stack(
+              children: [
+                Container(
+                  color: _currentColor,
+                  height: 500,
+                  width: 500,
+                ),
+                Container(
+                    color: Colors.white,
+                    height: 200,
+                    width: 200,
+                    child: Text(
+                      'You have pushed the button this many times:',
+                      style: TextStyle(
+                        fontSize: 20,
+                        color: _currentColor,
+                      ),
+                    ),
+                  )
+              ],
             ),
             Text(
               '$_counter',
@@ -40,6 +73,7 @@ class _HomePageState extends State<HomePage> {
         onPressed: _incrementCounter,
         tooltip: 'Increment',
         child: const Icon(Icons.add),
+        backgroundColor: _currentColor,
       ),
     );
   }
