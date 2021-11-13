0db9d6c6dc898ef9febbad404422c644e612c208
João Victor Perin <joaovperin@live.com>
2021-11-13 18:03:38 -0300

    added register page

--- aula_02/marketplace_app/lib/commons/colors.dart
+++ aula_02/marketplace_app/lib/commons/colors.dart
@@ -5,11 +5,13 @@ import 'package:flutter/material.dart';
 class AppColors {
   const AppColors._();
 
+  // static const green = Color(0xFF03A87C);
+
+  static const darkGrey = Color(0xFF3D4A5A);
+  static const lightGrey = Color(0xFF9B9B9B);
   static const black = Color(0xFF21252B);
-  static const lightGrey = Color(0xFF9CA5B4);
-  static const darkGrey = Color(0xFF5E656F);
   static const white = Color(0xFFFFFFFF);
-  static const green = Color(0xFF03A87C);
+  static const red = Color(0xFFE83F3F);
 
   static MaterialColor get primarySwatch => Colors.blue;
 }
--- aula_02/marketplace_app/lib/commons/routes.dart
+++ aula_02/marketplace_app/lib/commons/routes.dart
@@ -1,11 +1,11 @@
-import 'package:marketplace_app/pages/login_page.dart';
+import 'package:marketplace_app/pages/register_page.dart';
 
 class Routes {
   const Routes._();
 
-  static const initialRoute = LoginPage.routeName;
+  static const initialRoute = RegisterPage.routeName;
 
   static final routes = {
-    LoginPage.routeName: (context) => const LoginPage(),
+    RegisterPage.routeName: (context) => const RegisterPage(),
   };
 }
