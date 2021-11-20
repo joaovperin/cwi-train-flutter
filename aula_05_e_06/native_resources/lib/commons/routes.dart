60bdaa1e8907a8d6aeb1cb23aa1354fc1ad3f64c
João Victor Perin <joaovperin@live.com>
2021-11-20 15:07:06 -0300

    create app

---
+++ aula_05_e_06/native_resources/lib/commons/routes.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class AppRoutes {
+  const AppRoutes._();
+
+  // static const String initialRoute = SearchUserPage.routeName;
+  static const String initialRoute = '';
+
+  static final Map<String, WidgetBuilder> routes = {
+    // SearchUserPage.routeName: (context) => const SearchUserPage(),
+    // UserDetailsPage.routeName: (context) => UserDetailsPage(
+    //       ModalRoute.of(context)!.settings.arguments! as UserDetailsPageArgs,
+    //     ),
+  };
+}
