94fab462add8101cc25835a56a6c925595a3027a
João Victor Perin <joaovperin@live.com>
2021-11-27 09:41:08 -0300

    finish app

--- aula_08_e_09/timer_app/lib/pages/timer_page.dart
+++ aula_08_e_09/timer_app/lib/pages/timer_page.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 
 class TimerPage extends StatefulWidget {
@@ -8,15 +10,75 @@ class TimerPage extends StatefulWidget {
 }
 
 class _TimerPageState extends State<TimerPage> {
+  num timer = 0;
+  bool isRunning = false;
+
+  final _controller = StreamController<num>();
+  Stream<num> get _stream => _controller.stream;
+  StreamSink<num> get _sink => _controller.sink;
+
+  @override
+  initState() {
+    super.initState();
+    _sink.add(timer);
+
+    _stream.listen((num value) {
+      setState(() {
+        timer = value;
+      });
+    });
+  }
+
+  @override
+  void dispose() {
+    _controller.close();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Timer'),
       ),
-      body: const Center(
-        child: Text('Timer'),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const Text(
+              'Timer',
+              style: TextStyle(
+                fontSize: 48,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            const SizedBox(height: 20),
+            Text(
+              timer.toString(),
+              style: const TextStyle(
+                fontSize: 48,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            const SizedBox(height: 20),
+            ElevatedButton(
+              child: const Text('Start/Stop'),
+              onPressed: _onPressedStartStopBtn,
+            ),
+          ],
+        ),
       ),
     );
+  }
+
+  Future<void> _onPressedStartStopBtn() async {
+    isRunning = !isRunning;
+
+    while (true) {
+      await Future.delayed(const Duration(seconds: 1));
+      if (isRunning) {
+        _sink.add(timer + 1);
+      }
+    }
   }
 }
