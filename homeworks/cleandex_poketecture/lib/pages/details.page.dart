fb903d37edd59011ce2c9fb2c7f09c021449acea
João Victor Perin <joaovperin@live.com>
2021-12-02 16:38:48 -0300

    details page layout

--- homeworks/cleandex_poketecture/lib/pages/details.page.dart
+++ homeworks/cleandex_poketecture/lib/pages/details.page.dart
@@ -1,11 +1,14 @@
+import 'package:cleandex_poketecture/application/widgets/app_square_image_box.widget.dart';
 import 'package:cleandex_poketecture/commons/app_colors.dart';
 import 'package:flutter/material.dart';
+
+const bgGradientColors = AppColors.detailsPageGradient;
+const imageUrl =
+    'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/7.png';
 
 class DetailsPage extends StatefulWidget {
   static const routeName = '/details';
   const DetailsPage({Key? key}) : super(key: key);
-
-  final bgGradientColors = AppColors.detailsPageGradient;
 
   @override
   State<DetailsPage> createState() => _DetailsPageState();
@@ -21,25 +24,50 @@ class _DetailsPageState extends State<DetailsPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Container(
-        decoration: BoxDecoration(
+        decoration: const BoxDecoration(
           gradient: LinearGradient(
             begin: Alignment.topLeft,
             end: Alignment.topRight,
-            colors: widget.bgGradientColors,
+            colors: bgGradientColors,
           ),
         ),
         child: Column(
           children: [
-            const Spacer(),
-            Container(
-              height: MediaQuery.of(context).size.height * 0.8,
-              width: MediaQuery.of(context).size.width,
-              decoration: const BoxDecoration(
-                color: AppColors.cardColor,
-                borderRadius: BorderRadius.only(
-                  topLeft: Radius.circular(48),
-                  topRight: Radius.circular(48),
-                ),
+            Flexible(
+              flex: 10,
+              child: Stack(
+                children: [
+                  Column(
+                    children: [
+                      Flexible(flex: 4, child: Container()),
+                      Flexible(
+                        child: Container(
+                          decoration: const BoxDecoration(
+                            color: AppColors.cardColor,
+                            borderRadius: BorderRadius.only(
+                              topLeft: Radius.circular(48),
+                              topRight: Radius.circular(48),
+                            ),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                  const Align(
+                    alignment: Alignment.bottomCenter,
+                    child: SizedBox(
+                      width: 128,
+                      height: 128,
+                      child: SquareImageBoxWidget(imageUrl),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            Expanded(
+              flex: 31,
+              child: Container(
+                decoration: const BoxDecoration(color: AppColors.cardColor),
               ),
             ),
           ],
