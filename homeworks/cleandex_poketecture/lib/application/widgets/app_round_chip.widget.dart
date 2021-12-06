69f14c7e1bb5c15d413ae9fb5853e89ee70b764a
João Victor Perin <joaovperin@live.com>
2021-12-06 18:34:15 -0300

    show element images on moves info page

--- homeworks/cleandex_poketecture/lib/application/widgets/app_round_chip.widget.dart
+++ homeworks/cleandex_poketecture/lib/application/widgets/app_round_chip.widget.dart
@@ -1,3 +1,4 @@
+import 'package:cleandex_poketecture/commons/app_colors.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 
@@ -12,12 +13,13 @@ class AppRoundChipWidget extends StatelessWidget {
   final Color color;
   final String picturePath;
   final String? semanticsLabel;
+  final double containerSize = 36;
 
   @override
   Widget build(BuildContext context) {
     return Container(
-      width: 36,
-      height: 36,
+      width: containerSize,
+      height: containerSize,
       margin: const EdgeInsets.symmetric(horizontal: 4),
       padding: const EdgeInsets.all(8),
       decoration: BoxDecoration(
@@ -27,6 +29,64 @@ class AppRoundChipWidget extends StatelessWidget {
       child: SvgPicture.asset(
         picturePath,
         semanticsLabel: semanticsLabel,
+      ),
+    );
+  }
+}
+
+class AppRoundAssetImage extends StatelessWidget {
+  const AppRoundAssetImage(this.path, this.color, {Key? key}) : super(key: key);
+
+  final String path;
+  final Color color;
+  final double containerSize = 128;
+  final double imageSize = 64;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: containerSize,
+      height: containerSize,
+      alignment: Alignment.center,
+      decoration: BoxDecoration(
+        color: color,
+        boxShadow: const [
+          BoxShadow(
+            color: AppColors.containerShadow,
+            blurRadius: 6,
+            offset: Offset(0, -2),
+          ),
+        ],
+        shape: BoxShape.circle,
+      ),
+      child: SvgPicture.asset(
+        path,
+        fit: BoxFit.fill,
+        width: imageSize,
+        height: imageSize,
+        color: AppColors.container,
+      ),
+    );
+  }
+}
+
+class AppRoundNetworkImage extends StatelessWidget {
+  const AppRoundNetworkImage(this.url, {Key? key}) : super(key: key);
+
+  final String url;
+  final double containerSize = 128;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: containerSize,
+      height: containerSize,
+      alignment: Alignment.center,
+      decoration: BoxDecoration(
+        image: DecorationImage(
+          image: NetworkImage(url),
+          fit: BoxFit.fill,
+        ),
       ),
     );
   }
