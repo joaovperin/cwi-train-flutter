c5e72980389f9cf2716ddb37c16a87866796c20e
João Victor Perin <joaovperin@live.com>
2021-12-08 14:31:50 -0300

    applied tooltips to element chips

--- homeworks/cleandex_poketecture/lib/application/infra/abstract_http.mapper.dart
+++ homeworks/cleandex_poketecture/lib/application/infra/abstract_http.mapper.dart
@@ -7,13 +7,4 @@ abstract class AbstractHttpMapper<T> implements EntityMapper<T> {
     final idStr = RegExp(r'.*\/.*?\/(\d+)\/?').firstMatch(url)?.group(1) ?? '0';
     return int.parse(idStr);
   }
-
-  String formatName(String name) {
-    return name.split('-').map((e) {
-      if (e.length > 1) {
-        return e[0].toUpperCase() + e.substring(1);
-      }
-      return e;
-    }).join(' ');
-  }
 }
